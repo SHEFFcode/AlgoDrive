aa236a900e48e7fa087ae60addc4a0e3d0504288
Jeremy Shefer <sheffmachine@Jeremys-MacBook-Pro.local>
2017-06-12 18:02:14 -0700

    Start stepping numbers project'

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,8 @@ namespace Algorithms
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SteppingNumbers stepNum = new SteppingNumbers();
+            stepNum.SolveSteppingNumbers();
         }
     }
 }
---
+++ Algorithms/SteppingNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class SteppingNumbers
+    {
+        public void SolveSteppingNumbers()
+        {
+            //Given Numbers
+            var givenNumbers = new List<int>() { 10, 20 };
+
+
+
+
+        }
+    }
+}
